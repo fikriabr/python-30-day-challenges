dcd73ed17db5e38dcdd961a3ee659233f2a4fe66
Fikri Abdul Rahman <fikriabr96@gmail.com>
2025-08-09 12:00:00 +0700

    Day 18: Flask Migrate

---
+++ 18_flask_migrate/app/models/task_model.py
@@ -0,0 +1,14 @@
+from datetime import datetime
+from app import db
+
+class Task(db.Model):
+  __tablename__ = "tasks"
+
+  id = db.Column(db.Integer, primary_key=True)
+  title = db.Column(db.String(255), nullable=False)
+  description = db.Column(db.Text, nullable=True)
+  is_done = db.Column(db.Boolean, default=False)
+  created_at = db.Column(db.DateTime, default=datetime.utcnow)
+
+  def __repr__(self):
+    return f"<Task {self.title}>"
